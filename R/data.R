fbcdf259078ea03d5956abaac7910e44d0b84f2b
allydunham <ally@ebi.ac.uk>
2021-02-26 18:03:02 +0000

    Initial package structure

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Standardised deep mutational scanning studies results
+#'
+#' A dataset containing combined results from 28 deep mutational scanning
+#' studies. The fitness scores are transformed into the same format and
+#' normalised.
+#'
+#' @format A data frame with 6357 rows
+#' \describe{
+#'   \item{study}{Source study}
+#' }
+#'
+#' @source \href{https://www.biorxiv.org/content/10.1101/2020.05.26.116756v1}{Dunham and Beltrao (2020)}
+#"deepmutationalscans"
---
+++ R/deepscanscape.R
@@ -0,0 +1,16 @@
+#' deepscanscape: Check and Annotate Deep Mutational Scanning Data
+#'
+#' The deepscanscape package provides functions to standardise deep
+#' mutational scanning data and compare it to a deep mutational landscape
+#' calculated from a dataset of 28 previous studies. This allows the whole
+#' dataset to be checked for unusual properties and individual positions
+#' to be annotated with positional subtypes, indicating the positions in other
+#' proteins they are most similar to.
+#'
+#' @section Functions
+#'
+#' @docType package
+#' @name deepscanscape
+#' @import dplyr
+NULL
+#>NULL
