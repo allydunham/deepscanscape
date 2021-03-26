482d92da0503e4a73a626978a0c709bfa989c2ee
allydunham <ally@ebi.ac.uk>
2021-03-26 19:54:57 +0000

    v0.0.0.9024 Tidied pasring and standardisation
    * Added additional parsing (wide and sequences) and transformations (inverse, log2)
    * Added option to average over multiply mutated sequences for sequence and Mave input
    * Added full documentation for all functions, describing the parsing and transformations
      available.

--- R/parse.R
+++ R/parse.R
@@ -1,23 +1,38 @@
 # Parse common input formats in preparation for analysis
 # TODO - Test funcs
-# TODO - Add additional formats
-# TODO - support data with multiple scores for each variant
-# TODO - support data with multiple substitutions - take average of occurrence?
 
 #' Parse deep mutational scanning data
+#'
+#' Processes deep mutational scanning data from recognised formats and convert it into a standardised data frame, with
+#' one row per variant and position, wt, mut and score columns. Duplicate scores can also be averaged to account for
+#' multiple replicates and single variant scores averaged from multiply mutated sequences for some input formats.
+#'
+#' The following input formats are supported:
+#' \itemize{
+#'   \item \code{\link[=parse_mavedb]{mavedb}}: Data downloaded from \url{mavedb.org}. This format additionally supports
+#'   averaging over multiply mutated sequences.
+#'   \item long: Data in long format, which already has the required columns (position, wt, mut and score). This allows
+#'   duplicates to be averaged conveniently if data is already in the standard format.
+#'   \item \code{\link[=parse_wide]{wide}}: Process wide format data, with columns for position, wt and scores for each
+#'   amino acid
+#'   \item \code{\link[=parse_sequence]{sequence}}: Each score is associated with a sequence, from which variants are
+#'   extracted. This method supports averaging over multiply mutated sequences. It is the most frequent use for the
+#'   position_offset parameter.
+#' }
 #'
 #' @param x A tibble or an object that can be converted to one
 #' @param scheme Original data scheme (see description). Accepts any unambiguous
 #' substring.
-#' @param position_offset Offset all positions by this amount
+#' @param position_offset Offset all position
 #' @param duplicates How to handle duplicate scores for a single mutation
-#' @param ... Arguments passed to the chosen parsing function
-#'
+#' @param ... Arguments passed to the chosen parsing function, including specifying parsing for multiply mutated
+#' sequences (see individual methods for details).
+#' @return A long format \code{\link[tibble]{tibble}} with columns for 'position', 'wt', 'mut' and 'score'.
 #' @export
 parse_deep_scan <- function(x, scheme = c("mavedb", "long", "wide", "sequence"), position_offset = 0,
                             duplicates = c("warn", "mean", "median", "error"), ...) {
   scheme <- match.arg(scheme)
-  methods <- c("mavedb" = parse_mavedb, "long" = parse_long,
+  methods <- c("mavedb" = parse_mavedb, "long" = identity,
                "wide" = parse_wide, "sequence" = parse_sequence)
 
   x <- tibble::as_tibble(x)
@@ -43,20 +58,50 @@ parse_deep_scan <- function(x, scheme = c("mavedb", "long", "wide", "sequence"),
   return(x)
 }
 
-#' Parse data in the MaveDB format (called from parse_dms_data)
+#' Parse data in the MaveDB format
 #'
-#' @param x A tibble.
-#' @param score_col String. Column containing fitness scores.
+#' Parse data downloaded from \href{mavedb.org}{MaveDB}, including averaging across multiply mutated sequences.
+#' Usually called internally by\code{\link{parse_deep_scan}} but is exposed to help users get their input into the
+#' correct format.
+#'
+#' @param x A data frame with a column 'hgvs_pro' gicing the HGVS protein mutation string describing the variant(s) and
+#' a fitness score column ('score' by default).
+#' @param score_col String. Column containing fitness scores, to conveniently use an additional data column as the score
+#' where mutliple measurements are included in the data.
+#' @param average_multi Average scores for variants included in multiply mutated sequences, where they have not been
+#' measured individually. Care should be taken to check that the type of multiple mutation makes this appropriate.
 #' @param ... Ignored.
-
+#' @return A long format \code{\link[tibble]{tibble}} with columns specifying 'position', 'wt', 'mut' and 'score'.
 #' @export
-parse_mavedb <- function(x, score_col = "score", ...) {
-  x <- x[c("hgvs_pro", score_col)]
+parse_mavedb <- function(x, score_col = "score", average_multi = FALSE, ...) {
+  # Regexes
+  re_any_sub <- "p\\.\\[?([A-Z][a-z]{2}[0-9]*([A-Z][a-z]{2}|=);?)+\\]?"
+  re_multi_sub <- "p\\.\\[([A-Z][a-z]{2}[0-9]*([A-Z][a-z]{2}|=);?)*\\]"
+  re_single_sub <- "p\\.([A-Z][a-z]{2})([0-9]*)([A-Z][a-z]{2}|=|\\*)"
 
   # Filter tibble to substitutions
-  x <- dplyr::filter(x, stringr::str_detect(.data$hgvs_pro, "p\\.[A-Z][a-z]{2}[0-9]*([A-Z][a-z]{2}|=)"))
+  x <- tibble::as_tibble(x)
+  x <- dplyr::filter(x[c("hgvs_pro", score_col)], stringr::str_detect(.data$hgvs_pro, re_any_sub))
 
-  x[c("wt", "position", "mut")] <- stringr::str_match(x$hgvs_pro, "p\\.([A-Za-z=\\*]*)([0-9]*)([A-Za-z=\\*]*)")[, 2:4]
+  if (any(stringr::str_detect(x$hgvs_pro, re_multi_sub))) {
+    if (average_multi) {
+      x_multi <- dplyr::filter(x, stringr::str_detect(.data$hgvs_pro, re_multi_sub))
+      x_multi$hgvs_pro <- stringr::str_split(stringr::str_remove_all(x_multi$hgvs_pro, "(p\\.\\[|\\])"), ";")
+      x_multi <- tidyr::unnest(x_multi, .data$hgvs_pro)
+      x_multi$hgvs_pro <- stringr::str_c("p.", x_multi$hgvs_pro)
+      x_multi <- dplyr::summarise(dplyr::group_by(x_multi, .data$hgvs_pro), score = mean(.data$score))
+
+      x <- dplyr::filter(x, stringr::str_detect(.data$hgvs_pro, re_single_sub))
+      x <- dplyr::bind_rows(x, x_multi[!x_multi$hgvs_pro %in% x$hgvs_pro, ])
+
+    } else {
+      warning("Multiply mutated sequences detected. These are currently ignored, if you want to average these scores ",
+              "to use for variants that have not been measured alone use average_multi = TRUE", immediate. = TRUE)
+      x <- dplyr::filter(x, stringr::str_detect(.data$hgvs_pro, re_single_sub))
+    }
+  }
+
+  x[c("wt", "position", "mut")] <- stringr::str_match(x$hgvs_pro, re_single_sub)[, 2:4]
   x$wt <- unname(aa_3_to_1[stringr::str_to_lower(x$wt)])
   x$mut <- unname(aa_3_to_1[stringr::str_to_lower(x$mut)])
   x$position <- as.numeric(x$position)
@@ -64,26 +109,68 @@ parse_mavedb <- function(x, score_col = "score", ...) {
   return(x[c("position", "wt", "mut", "score")])
 }
 
-#' Parse data in the long format (called from parse_dms_data)
+#' Parse data in the wide format
 #'
-#' @param x A tibble.
-#' @param ... Ignored.
-parse_long <- function(x, ...) {
-  stop("Not implemented yet")
-}
-
-#' Parse data in the wide format (called from parse_dms_data)
+#' Parse wide format data into the standard layout. The input data frame must have columns specifying 'position',
+#' 'wt', and ER score ('score') for each amino acid (named 'A', 'C', ...). Usually called internally by
+#' \code{\link{parse_deep_scan}} but is exposed to help users get their input into the correct format.
 #'
-#' @param x A tibble.
+#' @param x A data frame.
 #' @param ... Ignored.
+#' @return A long format \code{\link[tibble]{tibble}} with columns specifying each variants 'position', 'wt', 'mut' and
+#' 'score'.
+#' @export
 parse_wide <- function(x, ...) {
-  stop("Not implemented yet")
+  x <- tibble::as_tibble(x)[, c("position", "wt", amino_acids)]
+  x <- tidyr::pivot_longer(x, dplyr::all_of(amino_acids), names_to = "mut", values_to = "score")
+  return(x)
 }
 
-#' Parse data in the sequence format (called from parse_dms_data)
+#' Parse data in the sequence format
 #'
-#' @param x A tibble.
+#' Extract variants from sequences and ER scores. The most frequent amino acid at each position in the sequence is
+#' assumed to be the wild type if a wild type sequence is not provided. Usually called internally by
+#' \code{\link{parse_deep_scan}} but is exposed to help users get their input into the correct format.
+#'
+#' @param x A data frame with columns 'sequence' and 'score'.
+#' @param wt_seq Character vector. Wild type sequence to calculate variants against. If NULL it is assumed to be the
+#' most common amino acid in each position.
+#' @param average_multi Average scores for variants included in multiply mutated sequences, where they have not been
+#' measured individually. Care should be taken to check that the type of multiple mutation makes this appropriate.
 #' @param ... Ignored.
-parse_sequence <- function(x, ...) {
-  stop("Not implemented yet")
+#' @return A long format data frame with columns for 'position', 'wt', 'mut' and 'score'.
+#' @export
+parse_sequence <- function(x, wt_seq = NULL, average_multi = FALSE, ...) {
+  x <- tibble::as_tibble(x)[, c("sequence", "score")]
+  seqs <- stringr::str_split(x$sequence, "", simplify = TRUE)
+
+  if (is.null(wt_seq)) {
+    wt_seq <- apply(seqs, 2, function(x) names(which.max(table(x))))
+  } else if (length(wt_seq) == 1) {
+    warning("length(wt_seq) == 1, splitting into a character vector with one amino acid per element")
+    wt_seq <- stringr::str_split(wt_seq, "", simplify = TRUE)
+  }
+
+  pos <- seq_along(wt_seq)
+  x$mut <- apply(seqs, 1, function(x) stringr::str_c(wt_seq, pos, x)[!x == wt_seq])
+  x <- tidyr::unnest(x, .data$mut)
+
+  x <- dplyr::add_count(x, .data$sequence)
+
+  if (any(x$n > 1)) {
+    if (average_multi) {
+      x_multi <- x[x$n > 1, ]
+      x_multi <- dplyr::summarise(dplyr::group_by(x_multi, .data$mut), score = mean(.data$score))
+      x <- x[x$n == 1, c("mut", "score")]
+      x <- dplyr::bind_rows(x, x_multi[!x_multi$mut %in% x$mut, ])
+
+    } else {
+      warning("Multiply mutated sequences detected. These are currently ignored, if you want to average these scores ",
+              "to use for variants that have not been measured alone use average_multi = TRUE", immediate. = TRUE)
+      x <- x[x$n == 1, c("mut", "score")]
+    }
+  }
+  x <- tidyr::extract(x, .data$mut, into = c("wt", "position", "mut"),
+                      regex = "([A-Z])([0-9]*)([A-Z\\*])", convert = TRUE)
+  return(x[c("position", "wt", "mut", "score")])
 }
--- R/standardise.R
+++ R/standardise.R
@@ -1,30 +1,44 @@
 # Normalise and standardise DMS scores
 # TODO - test functions
-# TODO - Add additional transforms
 
 #' Normalise deep mutational scanning fitness scores
+#'
+#' Normalise log2(ER) scores by dividing my the median of the lowest 10% of score, under the assumption these will
+#' represent complete protein knockout and will therefore be somewhat comparable between studies.
 #'
 #' @param x Vector of fitness scores
 #' @param q Quantile to normalise against. Values other than 0.1 differ from
 #' those used in the combined dataset
-#'
+#' @return Numeric vector of normalised ER scores
 #' @export
 normalise_er <- function(x, q=0.1) {
   q <- stats::quantile(x, q, na.rm = TRUE)
   return(x / -stats::median(x[x <= q], na.rm = TRUE))
 }
 
-#' Standardise DMS data
+#' Transform deep_mutational scanning scores onto a standard scale
+#'
+#' Transform deep mutational scanning fitness scores onto a standard scale aligned to the log2 enrichment ratio (ER)
+#' scale. This scale has neutral variants at 0, deleterious variants with negative scores and beneficial variants with
+#' positive scores. Standard transforms are provided and common transforms can be passed for more unusual ones.
+#'
+#' The supported transforms are:
+#' \itemize{
+#'   \item log2: log2 transform to change raw read ratios onto the log2 ER scale.
+#'   \item \code{\link[=transform_vamp]{vamp-seq}}: Transform scores produced by the VAMP-Seq method. These have the
+#'   median null mutation at 0, neutral variants at 1 and positive variants greater than 1.
+#'   \item invert: Multiply scores by -1, for example if the experimental fitness is inverted compared to natural
+#'   fitness
+#' }
 #'
 #' @param x Vector of fitness scores
-#' @param trans Transform to apply (see description). Accepts either a
-#' string or function
-#'
+#' @param trans Transform to apply (see description). Accepts either a string or a function.
+#' @return Numeric vector of transformed scores
 #' @export
-transform_er <- function(x, trans = c("log2", "vamp-seq", "unit")) {
+transform_er <- function(x, trans = c("log2", "vamp-seq", "invert")) {
   if (is.character(trans)) {
     trans <- match.arg(trans)
-    methods <- c("vamp-Seq" = transform_vamp, "unit" = transform_unit, "log2" = log2)
+    methods <- c("vamp-Seq" = transform_vamp, "log2" = log2, "invert" = function(x) -x)
     f <- methods[[trans]]
   } else if (is.function(trans)) {
     f <- trans
@@ -35,15 +49,15 @@ transform_er <- function(x, trans = c("log2", "vamp-seq", "unit")) {
   return(f(x))
 }
 
-#' Transform VAMP-seq data
-#' @keywords internal
-transform_vamp <- function(x, ...) {
+#' Transform VAMP-Seq fitness scores
+#'
+#' Transform VAMP-Seq fitness scores to the standard log2(ER) scale. The VAMP-Seq scores have the median null variant
+#' at 0, neutral variants at 1 and beneficial variant >1.
+#'
+#' @param x Vector of fitness scores
+#' @return Numeric vector of transformed scores
+#' @export
+transform_vamp <- function(x) {
   y <- 1 + (x - 1) / -min(x - 1, na.rm = TRUE)
   return(log2(y + min(y[y > 0], na.rm = TRUE)))
-}
-
-#' Transform unit scaled data
-#' @keywords internal
-transform_unit <- function(x, ...) {
-  stop("Not implemented yet")
 }
