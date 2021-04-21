3a89c8c12f998208e4d6475c6f0401b20576fb67
allydunham <ally@ebi.ac.uk>
2021-04-21 11:40:10 +0100

    v0.0.0.9037 Added tests for transformations

--- tests/testthat/test-standardise.R
+++ tests/testthat/test-standardise.R
@@ -4,3 +4,25 @@ test_that("normalisation works", {
   expect_equal(normalise_er(c(-4, -4, -2, -2, -1, 0, 1, 2, 2, 4, 4), q = 0.4),
                c(-2, -2, -1, -1, -0.5, 0, 0.5, 1, 1, 2, 2))
 })
+
+test_that("transform_vamp works", {
+  expect_equal(signif(transform_vamp(c(-0.05, 0, 0.05, 0.1, 0.5, 0.75, NA, 1, 1.5, 2)), 3),
+               c(-4.39, -3.39, -2.81, -2.39, -0.807, -0.305, NA, 0.0671, 0.608, 1))
+})
+
+test_that("transformation works", {
+
+  x <- c(0.1, 0.3, 0.5, NA, 1, 1.1)
+  expect_equal(transform_er(x, trans = "log2"), log2(x))
+
+  x <- c(-1, 0, 1, NA)
+  expect_equal(transform_er(x, trans = "invert"), -x)
+
+  x <- c(-0.05, 0, 0.05, 0.1, 0.5, 0.75, NA, 1, 1.5, 2)
+  expect_equal(transform_er(x, trans = "vamp"), transform_vamp(x))
+
+  expect_error(transform_er(1, trans = "Unrecognised_String"),
+               "'arg' should be one of (\".*\",? ?)*")
+  expect_error(transform_er(1, trans = NA),
+               "Unrecognised transformation\\. Pass a supported string or a function")
+})
